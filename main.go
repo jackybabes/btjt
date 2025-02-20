cd95b8afa1feada34d577664207f1c140d7c24d3
jackybabes <jackcbtricker@gmail.com>
2025-02-20 18:35:13 +0000

    sends handshake to peer and recives (if peer working)

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/url"
+	"slices"
 	"strconv"
+	"time"
 )
 
 func parsePeerBytesIntoPeerList(peerBytes []byte) []Peer {
@@ -34,6 +38,62 @@ func parsePeerBytesIntoPeerList(peerBytes []byte) []Peer {
 
 	}
 	return peerList
+}
+
+func sendHandshakeToPeer(p Peer, infoHash [20]byte) {
+	// peerAddress := "188.212.112.163:32767"
+
+	peerAddress := fmt.Sprintf("%v:%v", p.IP, p.Port)
+	log.Println(peerAddress)
+
+	conn, err := net.DialTimeout("tcp", peerAddress, time.Second)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	log.Printf("connected to %v", peerAddress)
+
+	peerID := make([]byte, 20)
+	rand.Read(peerID)
+
+	// length of the protocol string (BitTorrent protocol) which is 19 (1 byte)
+	handshakeLength := []byte{19}
+	// the string BitTorrent protocol (19 bytes)
+	handshakeProtocolString := []byte("BitTorrent protocol")
+	// eight reserved bytes, which are all set to zero (8 bytes)
+	handshakeNullBytes := make([]byte, 8)
+	// sha1 infohash (20 bytes) (NOT the hexadecimal representation, which is 40 bytes long)
+	handshakeInfoHash := infoHash[:]
+	// peer id (20 bytes) (generate 20 random byte values)
+	handshakePeerID := peerID
+
+	handshake := slices.Concat(handshakeLength, handshakeProtocolString, handshakeNullBytes, handshakeInfoHash, handshakePeerID)
+
+	log.Printf("Handshake len : %v", len(handshake))
+
+	_, err = conn.Write(handshake)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Handshake sent")
+
+	response := make([]byte, 68)
+
+	conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	n, err := conn.Read(response)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(n)
+
+	log.Printf("%x", response[68-20:])
+	return
 }
 
 func main() {
@@ -121,6 +181,12 @@ func main() {
 	peerList := parsePeerBytesIntoPeerList(trackerResponseUnpacked.Peers)
 
 	_ = peerList
+
+	// handshake
+
+	handshakePeer := peerList[0]
+
+	sendHandshakeToPeer(handshakePeer, infoHash)
 
 	log.Println("Fin")
 

6051be48b17486adfc18ea6f3888f1aa83882154
jackybabes <jackcbtricker@gmail.com>
2025-03-18 14:14:16 +0000

    connect to a couple of peers for testing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sync"
 )
 
 const (
@@ -16,6 +17,41 @@ func main() {
 
 	torrent := NewTorrent(TORRENT_FILE_PATH)
 	torrent.initialiseTrackers()
+
+	// for testing reduce the number of peers
+	if len(torrent.Peers) > 10 {
+		// Create a temporary map to store the first 10 peers
+		tempPeers := make(map[string]*Peer)
+		count := 0
+		for k, v := range torrent.Peers {
+			if count >= 10 {
+				break
+			}
+			tempPeers[k] = v
+			count++
+		}
+		torrent.Peers = tempPeers
+	}
+
+	// init peers using goroutines
+	var wg sync.WaitGroup
+	for _, peer := range torrent.Peers {
+		wg.Add(1)
+		go func(p *Peer) {
+			defer wg.Done()
+			log.Printf("peer: %v", p.Name)
+			p.Init(torrent.InfoHash)
+		}(peer)
+	}
+	wg.Wait()
+
+	// Remove peers that are not alive for testing
+	for key, peer := range torrent.Peers {
+		if !peer.Alive {
+			delete(torrent.Peers, key)
+		}
+	}
+	log.Printf("Remaining alive peers: %d", len(torrent.Peers))
 
 	log.Println("Fin")
 

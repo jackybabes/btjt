55b60e9fa20617e12d1e23764763c9b7cfac3f88
jackybabes <jackcbtricker@gmail.com>
2025-03-18 14:14:05 +0000

    add code to connect to peer and handhake

--- peer.go
+++ peer.go
@@ -6,16 +6,21 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"slices"
 	"time"
 )
 
 type Peer struct {
-	PeerID     []byte
-	Name       string
-	IP         net.IP
-	Port       int
-	Alive      bool
-	Connection net.Conn
+	PeerID         []byte
+	Name           string
+	IP             net.IP
+	Port           int
+	Alive          bool
+	Connection     net.Conn
+	peerAddress    string
+	handshakeMsg   []byte
+	Bitfield       []byte
+	BitfieldLength uint32
 }
 
 func NewPeerCompactIpv4(b [6]byte) Peer {
@@ -32,23 +37,110 @@ func NewPeerCompactIpv4(b [6]byte) Peer {
 	return p
 }
 
-func (p *Peer) Init() {
-	p.CreateConnection()
+// func NewPeerCompactIpv6(b [16]byte) Peer {
 
-	defer p.Connection.Close()
+func (p *Peer) Init(infoHash [20]byte) {
+	// Set peer address string
+	p.peerAddress = fmt.Sprintf("%v:%v", p.IP.String(), p.Port)
+
+	// Create Handshake message
+	p.createHandshakeMessage(infoHash)
+	p.SendHandshake()
+
+	// p.CreateConnection()
+
+	// defer p.Connection.Close()
 }
 
-func (p *Peer) CreateConnection() {
+func (p *Peer) createHandshakeMessage(infoHash [20]byte) {
+	handshakeLength := []byte{19}
+	handshakeProtocolString := []byte("BitTorrent protocol")
+	handshakeNullBytes := make([]byte, 8)
+	handshakeInfoHash := infoHash[:]
+	handshakePeerID := p.PeerID
 
-	peerAddress := fmt.Sprintf("%v:%v", p.IP.String(), p.Port)
-	conn, err := net.DialTimeout("tcp", peerAddress, time.Second)
-	if err != nil {
-		log.Println(err)
+	handshake := slices.Concat(handshakeLength, handshakeProtocolString, handshakeNullBytes, handshakeInfoHash, handshakePeerID)
+
+	p.handshakeMsg = handshake
+}
+
+func (p *Peer) SendHandshake() {
+	// Create connection and store it in the struct
+	conn := p.CreateConnection()
+	if conn == nil {
+		p.Alive = false
 		return
 	}
-	p.Connection = conn
-	log.Printf("connected to %v", peerAddress)
+	p.Connection = conn // Store the connection in the struct instead of closing it
 
+	_, err := conn.Write(p.handshakeMsg)
+	if err != nil {
+		log.Println(err)
+		p.Alive = false
+		p.Connection.Close()
+		return
+	}
+
+	log.Println("Handshake sent")
+
+	response := make([]byte, 68)
+
+	conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	_, err = conn.Read(response)
+	if err != nil {
+		log.Println(err)
+		p.Alive = false
+		p.Connection.Close()
+		return
+	}
+
+	// Remove the deadline after handshake
+	conn.SetReadDeadline(time.Time{})
+
+	log.Printf("Handshake response: %v", response)
+
+	// Read bitfield message
+	lengthBuf := make([]byte, 4)
+	_, err = conn.Read(lengthBuf)
+	if err != nil {
+		log.Println("Failed to read bitfield length:", err)
+		p.Alive = false
+		p.Connection.Close()
+		return
+	}
+
+	p.BitfieldLength = binary.BigEndian.Uint32(lengthBuf)
+
+	// First byte is message ID (5 for bitfield)
+	messageBuf := make([]byte, p.BitfieldLength)
+	_, err = conn.Read(messageBuf)
+	if err != nil {
+		log.Println("Failed to read bitfield:", err)
+		p.Alive = false
+		p.Connection.Close()
+		return
+	}
+
+	if messageBuf[0] != 5 { // 5 is the ID for bitfield messages
+		log.Println("Expected bitfield message, got message ID:", messageBuf[0])
+		p.Alive = false
+		p.Connection.Close()
+		return
+	}
+
+	p.Bitfield = messageBuf[1:] // The actual bitfield data starts after the message ID
+	log.Printf("Received bitfield of length %d: %v", len(p.Bitfield), p.Bitfield)
+	p.Alive = true // Mark as alive if we got here successfully
+}
+
+func (p *Peer) CreateConnection() net.Conn {
+	conn, err := net.DialTimeout("tcp", p.peerAddress, time.Second)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	log.Printf("connected to %v", p.peerAddress)
+	return conn
 }
 
 // func (p *Peer) SendHandshake() {
@@ -89,3 +181,11 @@ func (p *Peer) CreateConnection() {
 // 	log.Printf("%x", response[68-20:])
 // 	return
 // }
+
+func (p *Peer) Close() {
+	if p.Connection != nil {
+		p.Connection.Close()
+		p.Connection = nil
+	}
+	p.Alive = false
+}

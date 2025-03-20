804fce93ec91052973aba2c8a26d0736ce11488b
jackybabes <jackcbtricker@gmail.com>
2025-03-20 18:18:41 +0000

    add choked varible

--- peer.go
+++ peer.go
@@ -21,27 +21,33 @@ type Peer struct {
 	handshakeMsg   []byte
 	Bitfield       []byte
 	BitfieldLength uint32
+	Choked         bool
 }
 
 func NewPeerCompactIpv4(b [6]byte) Peer {
-	ipstr := fmt.Sprintf("%v.%v.%v.%v", b[0], b[1], b[2], b[3])
-	ip := net.ParseIP(ipstr)
-	port := int(binary.BigEndian.Uint16(b[4:]))
+	// Create the Peer instance at the beginning
+	p := Peer{}
 
-	peerstr := fmt.Sprintf("%v:%v", ipstr, port)
+	ipstr := fmt.Sprintf("%v.%v.%v.%v", b[0], b[1], b[2], b[3])
+	p.IP = net.ParseIP(ipstr)
+	p.Port = int(binary.BigEndian.Uint16(b[4:]))
+	p.Name = fmt.Sprintf("%v:%v", ipstr, p.Port)
 
 	peerID := make([]byte, 20)
 	rand.Read(peerID)
+	p.PeerID = peerID
 
-	p := Peer{IP: ip, Port: port, Name: peerstr, PeerID: peerID}
+	// Set peer address string
+	p.peerAddress = fmt.Sprintf("%v:%v", p.IP.String(), p.Port)
+
+	p.Choked = true
+
 	return p
 }
 
 // func NewPeerCompactIpv6(b [16]byte) Peer {
 
 func (p *Peer) Init(infoHash [20]byte) {
-	// Set peer address string
-	p.peerAddress = fmt.Sprintf("%v:%v", p.IP.String(), p.Port)
 
 	// Create connection and store it in the struct
 	conn := p.CreateConnection()
@@ -58,99 +64,6 @@ func (p *Peer) Init(infoHash [20]byte) {
 	// p.CreateConnection()
 
 	// defer p.Connection.Close()
-}
-
-func (p *Peer) sendInterested() {
-	messageLen := []byte{0, 0, 0, 1}
-	messageID := []byte{2}
-	message := slices.Concat(messageLen, messageID)
-	log.Printf("Sending interested: %v", message)
-	_, err := p.Connection.Write(message)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func (p *Peer) receiveUnchoke() {
-	response := make([]byte, 5)
-	_, err := p.Connection.Read(response)
-	if err != nil {
-		log.Println(err)
-		p.Alive = false
-		p.Connection.Close()
-		return
-	}
-	log.Printf("Received unchoke: %v", response)
-}
-
-func (p *Peer) sendRequest(peiceIndexInt int, blockOffsetInt int) {
-	// message length
-	messageLen := []byte{0, 0, 0, 13}
-
-	// message ID
-	messageID := []byte{6}
-
-	// piece index
-	pieceIndex := make([]byte, 4)
-	binary.BigEndian.PutUint32(pieceIndex, uint32(peiceIndexInt))
-
-	// block offset
-	blockOffset := make([]byte, 4)
-	binary.BigEndian.PutUint32(blockOffset, uint32(blockOffsetInt))
-
-	// block length
-	blockLength := make([]byte, 4)
-	binary.BigEndian.PutUint32(blockLength, uint32(BLOCK_SIZE))
-
-	// message
-	message := slices.Concat(messageLen, messageID, pieceIndex, blockOffset, blockLength)
-
-	log.Printf("Sending request: %v", message)
-	_, err := p.Connection.Write(message)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func (p *Peer) receivePiece() {
-	log.Printf("Receiving piece")
-
-	// First read the message length (4 bytes)
-	lengthBuf := make([]byte, 4)
-	_, err := p.Connection.Read(lengthBuf)
-	if err != nil {
-		log.Println("Failed to read piece message length:", err)
-		p.Alive = false
-		p.Connection.Close()
-		return
-	}
-
-	messageLength := binary.BigEndian.Uint32(lengthBuf)
-	log.Printf("Message length: %d", messageLength)
-
-	// Read the actual message
-	response := make([]byte, messageLength)
-	_, err = p.Connection.Read(response)
-	if err != nil {
-		log.Println("Failed to read piece message:", err)
-		p.Alive = false
-		p.Connection.Close()
-		return
-	}
-
-	if response[0] != 7 {
-		log.Printf("Expected piece message (ID=7), got message ID: %d", response[0])
-		return
-	}
-
-	// Parse piece message
-	pieceIndex := binary.BigEndian.Uint32(response[1:5])
-	blockOffset := binary.BigEndian.Uint32(response[5:9])
-	blockData := response[9:]
-
-	log.Printf("Piece index: %d, Block offset: %d, Block data length: %d",
-		pieceIndex, blockOffset, len(blockData))
-	log.Printf("First few bytes of block data: %v", blockData[:min(10, len(blockData))])
 }
 
 func (p *Peer) createHandshakeMessage(infoHash [20]byte) {
